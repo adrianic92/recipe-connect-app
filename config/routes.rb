ecd360e3970a00edd82b60f06674eec859d4d31f
Adrian <adrianic92@yahoo.com>
2023-08-22 19:45:16 -0700

    updated sessions controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   resources :recipe_tags
   resources :tags
   resources :recipes
-  resources :users
+  # resources :users
+
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
