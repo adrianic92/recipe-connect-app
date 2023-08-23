1a58917f71c41463088f921b4d914707cf6f25dc
Adrian <adrianic92@yahoo.com>
2023-08-22 20:20:39 -0700

    added users#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,25 @@
 class UsersController < ApplicationController
+    skip_before_action :authorize, only: [:create, :index]
+    
     def show
         render json: @current_user
     end
+
+    def index
+        users = User.all
+        render json: users
+    end
+
+    def create
+        user = User.create!(user_params)
+        session[:user_id]
+        render json: user, status: :created
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation, :name, :bio, :date_of_birth)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
   get "/me", to: "users#show"
+  post "/signup", to: "users#create"
+  get "/all", to: "users#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
