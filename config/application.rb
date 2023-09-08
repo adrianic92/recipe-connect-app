590d26b639240cd7ad0456376601fe549eeca66f
Adrian <adrianic92@yahoo.com>
2023-09-08 08:37:39 -0700

    removed require pry

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,6 @@ require "rails/test_unit/railtie"
 
 require "date"
 
-require "pry"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
